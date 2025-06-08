d028af31f4ac67c4a4541c65fc6a3fc0b304f9ee
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-08 18:51:38 +0100

    Feature: Add social media contact information

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,9 @@ function Contact() {
     contactHeading.textContent = "Contact";
 
     // From Far
-    let offSite = document.createElement("article");
+    let online = document.createElement("article");
+    let onlineHeading = document.createElement("h3");
+    onlineHeading.textContent = "Online Contact"
     let email = document.createElement("p");
     email.textContent = "You can email us at contat@restaurant.com"
     let mail = document.createElement("p");
@@ -14,12 +16,12 @@ function Contact() {
     let phone = document.createElement("p");
     phone.textContent = "You can call on the number 01234567890";
 
-    offSite.appendChild(email);
-    offSite.appendChild(mail);
-    offSite.appendChild(phone);
+    online.appendChild(email);
+    online.appendChild(mail);
+    online.appendChild(phone);
 
     // In Person
-    let onSite = document.createElement("article");
+    let inPerson = document.createElement("article");
     let mainBranch = document.createElement("p");
     mainBranch.textContent =
             "You can visit our main branch at 00, Happy Lane, Queensland,"
@@ -33,13 +35,38 @@ function Contact() {
         "You can visit our first sub branch at 00, Happy Lane, Queensland,"
             + " Antartica.";
 
-    onSite.appendChild(mainBranch);
-    onSite.appendChild(firstSubBranch);
-    onSite.appendChild(secondSubBranch);
+    inPerson.appendChild(mainBranch);
+    inPerson.appendChild(firstSubBranch);
+    inPerson.appendChild(secondSubBranch);
+
+    // Social
+    let social = document.createElement("article");
+    let socialHeading = document.createElement("h3");
+    socialHeading.textContent = "Social Media";
+    let socialText = document.createElement("p");
+    socialText.textContent =
+            "You can follow us on the following social media websites:"
+    let socialList = document.createElement("ul");
+    let socialFacebook = document.createElement("li");
+    socialFacebook.textContent = "Facebook";
+    let socialTwitter = document.createElement("li");
+    socialTwitter.textContent = "Twitter";
+    let socialInstragram = document.createElement("li");
+    socialInstragram.textContent = "Instagram";
+    let socialSnapchat = document.createElement("li");
+    socialSnapchat.textContent = "Snapchat";
+    socialList.appendChild(socialFacebook)
+                .appendChild(socialTwitter)
+                .appendChild(socialInstragram)
+                .appendChild(socialSnapchat);
+    social.appendChild(socialHeading);
+    social.appendChild(socialText);
+    social.appendChild(socialList);
 
     contact.appendChild(contactHeading);
-    contact.appendChild(offSite);
-    contact.appendChild(onSite);
+    contact.appendChild(online);
+    contact.appendChild(inPerson);
+    contact.appendChild(social);
 
     let main = document.getElementsByTagName("main")[0];
     main.appendChild(contact);
